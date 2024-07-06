852396c078ccef99329e9221d3357d0e6395422c
Aworetan Michael <mvillacs07@gmail.com>
2024-07-06 05:47:07 +0100

    added routes to the app entry point

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db'); // Import database connection function
+// Import task routes
+const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
@@ -12,5 +14,18 @@ app.use(bodyParser.json()); // Parse JSON request bodies
 // Connect to the database
 connectDB();
 
+// Use task routes for handling API requests
+app.use('/api', taskRoutes);
+
+// Root endpoint for health check
+app.get('/', (req, res) => {
+    res.send('Task management API is running');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: err.message });
+});
 
 module.exports = app;
